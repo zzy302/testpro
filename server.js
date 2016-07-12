53d0841b679f580d6c4b8bc4f3a224e820d8830d
zzy302 <zycs@163.com>
2016-07-12 16:43:36 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("Hello World! 062112");
+  response.end("Hello World! 07121643");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
