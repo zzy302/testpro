d0bcea98bc0ff8bc210ec863a571c671cac0f386
zzy302 <zycs@163.com>
2017-01-04 14:35:32 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("Hello World! ----fgccg----");
+  response.end("Hello World! ----fg-ccg----");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
